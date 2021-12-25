0a414ee881b2f5fc4d7ef32d1d682085d3e523c3
Xuwei Li <dev@xuwei.li>
2021-12-25 12:47:43 +0800

    Finish tutorials: https://developer.apple.com/tutorials/swiftui/creating-and-combining-views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Xuwei Li on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
